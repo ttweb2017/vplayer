e8f468bfb5818396141ce87ff349f8eabaed6312
shgini27 <go_shuhrat@yahoo.com>
2019-12-01 00:04:47 +0500

    camera screen record button

--- lib/camera_player_screen.dart
+++ lib/camera_player_screen.dart
@@ -108,7 +108,7 @@ class _CameraScreenState extends State<CameraScreen> with WidgetsBindingObserver
                 ),
               ),
               Positioned.fill(
-                top: height - 420.0,
+                top: height - 440.0,
                 child: Align(
                   alignment: Alignment.bottomCenter,
                   child:  _recordControlWidget(),
